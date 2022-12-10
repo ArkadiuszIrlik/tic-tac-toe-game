1c22a0443ab4682a8f5ce2e4759b2dd06ec79d41
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2022-12-10 17:59:29 +0100

    Add gameController module
    This code adds the foundation of the gameController object which contains
    most of the 'behind-the-scenes' logic of the game.
    
    It can generate a new game board based on provided size parameters, it
    starts and ends the game as well as each individual turn. Finally, it
    also handles the player objects, assigning them 'markers' they will play
    the game with and adds wins to their win counter after the game is over.

--- script.js
+++ script.js
@@ -10,3 +10,110 @@ const Player = function(name, avatar) {
     getWins,
   }
 }
+
+const gameController = (function () {
+  const gameParameters = {
+    columns: 3,
+    rows: 3,
+    winningStreak: 3,
+    turnTimer: 5000,
+  }
+
+  let gameBoard;
+  let currentPlayers = [];
+  let activePlayer;
+  let turnCounter = 0;
+  let turnTimerID;
+
+  const createBoardSquare = () => {
+    const emptySquare = document.createElement('div');
+    emptySquare.dataset.marker = '';
+    emptySquare.classList.add('board-square', 'active');
+    emptySquare.addEventListener('click', endTurn);
+    return emptySquare;
+  }
+
+  const createBoard = (parameters) => {
+    const emptySquare = createBoardSquare();
+    const board = [];
+
+    for (let row = 0; row < parameters.rows; row++) {
+      board.push([]);
+      for (let column = 0; column < parameters.columns; column++) {
+        const squareInstance = emptySquare.cloneNode();
+        board[row].push(squareInstance);
+        displayController.addSquare(squareInstance);
+      }
+    }
+
+    return board;
+  }
+
+  const startGame = () => {
+    gameBoard = createBoard(gameParameters);
+    assignRandomMarkers();
+    activePlayer = currentPlayers[Math.floor(Math.random() * 2)];
+    startNewTurn();
+  }
+
+  const startNewTurn = () => {
+    turnCounter++;
+    displayController.displayNewTurn(activePlayer, turnCounter);
+    turnTimerID = startTurnTimer();
+  }
+
+  const endTurn = (event) => {
+    stopTurnTimer();
+    event.target.dataset.marker = activePlayer.marker;
+    event.target.classList.remove('active');
+    if (isWinner()) {
+      endGame();
+    } else {
+      changeActivePlayer();
+      startNewTurn();
+    }
+  }
+
+  const changeActivePlayer = () => {
+    activePlayer = (activePlayer == currentPlayers[0]) ? 
+        currentPlayers[1] : currentPlayers[0];
+  }
+
+  const startTurnTimer = () => {
+    if (gameParameters.turnTimer == Infinity) {
+      return null;
+    } else {
+    setTimeout(failPlayerTimeout, gameParameters.turnTimer);
+    }
+  }
+
+  const stopTurnTimer = () => {
+    clearTimeout(turnTimerID);
+  }
+
+  const endGame = () => {
+    activePlayer.addWin();
+    displayController.displayWinner(activePlayer);
+    activePlayer = null;
+    gameBoard = [];
+    currentPlayer.forEach(player => player.dataset.marker = '');
+  }
+
+  const addPlayer = (player) => {
+    currentPlayers.push(player);
+  }
+
+  const failPlayerTimeout = () => {
+    displayController.displayPlayerTimeout();
+    changeActivePlayer();
+    endGame();
+  }
+
+  const assignRandomMarkers = () => {
+    const availableMarkers = ['cross', 'circle'];
+    currentPlayers[0].dataset.marker = availableMarkers.splice(
+      Math.floor(Math.random() * 2), 1)[0];
+    currentPlayers[1].dataset.marker = availableMarkers[0];
+  }
+
+})();
